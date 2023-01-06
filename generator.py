c111d2eded1f261d23ba01ad1a410668e99c1791
Giandomenico D'Onofrio <45636384+GiandomenicoDOnofrio@users.noreply.github.com>
2023-01-06 19:58:18 +0000

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,14 +1,20 @@
-big_title = "Big title"
-headers = ["header 1", "header 2", "header 3", "header 4"]
+big_title = "Morphological arrhythmias systems"
+headers = ["Author", "Performance", "Database", "Classes"]
 data = [
-    ["Data 1", "Data 2", "3", "Data 4"],
-    ["Data 1", "Data 2", "3", "Data 4"],
+    ["Raj and Ray (2018)", "Acc 0.963 \n F-score 0.7606", "MIT-BIH", "16"],
+    ["Mousavi and Afghah(2019)", "Acc  0.9992", "MIT-BIH", "6"]
 ]
 
-res = f'\\begin{{tabular}}{{{"".join(["|p{3cm}|" for i in range(len(headers))])}}} \n\hline \n\multicolumn{{{len(headers)}}}{{|c|}}{{{big_title}}} \\\\ \n\hline \n{" & ".join(headers)} \\\\ \n\hline \n'
+res = f'\\begin{{tabular}}{{{"".join(["|p{3cm}|" for i in range(len(headers))])}}} '
+res += f'\n\hline \n'
+res += f'\multicolumn{{{len(headers)}}}{{|c|}}{{{big_title}}} \\\\ '
+res += f'\n\hline \n'
+res += f'{" & ".join(headers)} \\\\ \n'
+res += f'\hline \n'
 
 for i, row in enumerate(data):
-    res+= f'{" & ".join(row)}'
+    v = ['\parbox[t]{3cm}{' + item.replace("\n", "\\\\") + '}' for item in row]
+    res+= f'{" & ".join(v)}'
     res+= f' \\\\ \hline \n'
 
 res += f'\\end{{tabular}}'
